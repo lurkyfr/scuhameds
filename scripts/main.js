7e4686134d55eb7f9b7dfd13085be5895afd5331
lurkyfr <116358218+lurkyfr@users.noreply.github.com>
2025-09-24 12:36:58 -0500

    d

--- scripts/main.js
+++ scripts/main.js
@@ -153,7 +153,7 @@ const GameUtils = {
             src: gameData.src,
             title: gameData.title
         });
-        window.location.href = `./focus?${params.toString()}`;
+        window.location.href = `../focus?${params.toString()}`;
     },
 
     // Format player count
